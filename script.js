0007537622d08ab3b67e7b6d411c3f2ae7a9697c
damiang0 <damian.michel@bbh-cfp.ch>
2022-12-18 17:55:18 +0100

    gegner

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
-var spieler = document.querySelector(".player");
-var background = document.querySelector(".space");
+const spieler = document.querySelector(".player");
+const background = document.querySelector(".space");
 
 spieler.style.left = "400px";
 spieler.style.top = "500px";
 
-var timer_enemy = new Timer(30);
-var timer_projectile = new Timer(30);
+const timer_enemy = new Timer(33);
+const timer_projectile = new Timer(30);
 
-var score = 0;
-var scorepoints = document.querySelector(".score");
+let score = 0;
+const scorepoints = document.querySelector(".score");
+
+const main = document.querySelector("main");
 
 function Steuerung() {
   if (keyboard(39)) {
-    spieler.style.left = parseInt(spieler.style.left) + 5 + "px";
+    spieler.style.left = parseInt(spieler.style.left) + 6 + "px";
+
+    /* Spielberich eingrenzen 
+    if (spieler.style.left >= 500) {
+      spieler.style.left = parseInt(spieler.style.left) - 7 + "px";
+    }
+    */
   }
   if (keyboard(37)) {
-    spieler.style.left = parseInt(spieler.style.left) - 5 + "px";
+    spieler.style.left = parseInt(spieler.style.left) - 6 + "px";
+
+    /* Spielberich eingrenzen 
+    if (spieler.style.left <= 300) {
+      spieler.style.left + 7 + "px";
+    }
+    */
   }
 }
 
 function Gegner() {
   if (timer_enemy.ready()) {
-    var e = document.createElement("div");
+    var e = document.createElement("img");
+    e.setAttribute("src", "enemy.png");
     e.classList.add("enemy");
     e.style.top = "0px";
     e.style.left = "0px";
@@ -30,20 +45,68 @@ function Gegner() {
 
   var enemys = document.querySelectorAll(".enemy");
 
+  //Bewegung Gegner (Zickzack Linie)
   for (var enemy of enemys) {
-    enemy.style.left = parseInt(enemy.style.left) + 5 + "px";
-    if (parseInt(enemy.style.left) > 780) {
-      enemy.style.left = parseInt(enemy.style.left) - 5 + "px";
+    //links
+    if (parseInt(enemy.style.left) < 780 && parseInt(enemy.style.top) == 0) {
+      enemy.style.left = parseInt(enemy.style.left) + 5 + "px";
+    }
+
+    //runter (rechte Seite)
+    if (parseInt(enemy.style.left) >= 780 && parseInt(enemy.style.top) < 100) {
       enemy.style.top = parseInt(enemy.style.top) + 2 + "px";
     }
-    if (parseInt(enemy.style.top) > 20) {
+
+    //rechts
+    if (parseInt(enemy.style.left) > 0 && parseInt(enemy.style.top) == 100) {
       enemy.style.left = parseInt(enemy.style.left) - 5 + "px";
-      enemy.style.top = parseInt(enemy.style.top) - 2 + "px";
+    }
+
+    //runter (linke Seite)
+    if (parseInt(enemy.style.left) == 0 && parseInt(enemy.style.top) <= 200) {
+      enemy.style.top = parseInt(enemy.style.top) + 2 + "px";
+    }
+
+    if (parseInt(enemy.style.left) < 780 && parseInt(enemy.style.top) == 200) {
+      enemy.style.left = parseInt(enemy.style.left) + 5 + "px";
+    }
+
+    if (
+      parseInt(enemy.style.left) >= 780 &&
+      parseInt(enemy.style.top) < 300 &&
+      parseInt(enemy.style.top) > 200
+    ) {
+      enemy.style.top = parseInt(enemy.style.top) + 5 + "px";
+    }
+
+    if (parseInt(enemy.style.left) > 0 && parseInt(enemy.style.top) == 300) {
+      enemy.style.left = parseInt(enemy.style.left) - 5 + "px";
+    }
+
+    if (
+      parseInt(enemy.style.left) == 0 &&
+      parseInt(enemy.style.top) <= 400 &&
+      parseInt(enemy.style.top) > 200
+    ) {
+      enemy.style.top = parseInt(enemy.style.top) + 2 + "px";
+    }
+
+    if (parseInt(enemy.style.left) < 780 && parseInt(enemy.style.top) == 400) {
+      enemy.style.left = parseInt(enemy.style.left) + 5 + "px";
+    }
+
+    if (parseInt(enemy.style.left) >= 780 && parseInt(enemy.style.top) < 500) {
+      enemy.style.top = parseInt(enemy.style.top) + 2 + "px";
+    }
+
+    if (parseInt(enemy.style.left) > 0 && parseInt(enemy.style.top) == 500) {
+      enemy.style.left = parseInt(enemy.style.left) - 5 + "px";
     }
   }
 
   if (anyCollision(spieler, enemys)) {
-    alert("Game over!");
+    main.innerHTML = " ";
+    alert("Game over! Please restart your page!");
     return;
   }
 }
@@ -66,7 +129,8 @@ function Schuss() {
   var spx = spieler.style.left;
 
   if (timer_projectile.ready()) {
-    var p = document.createElement("div");
+    var p = document.createElement("img");
+    p.setAttribute("src", "projectile.png");
     p.classList.add("projectile");
     p.style.top = "500px";
     p.style.left = spx;
@@ -76,7 +140,7 @@ function Schuss() {
   var projectiles = document.querySelectorAll(".projectile");
 
   for (var projectile of projectiles) {
-    projectile.style.top = parseInt(projectile.style.top) - 2 + "px";
+    projectile.style.top = parseInt(projectile.style.top) - 4 + "px";
     if (parseInt(projectile.style.top) < 0) {
       projectile.parentNode.removeChild(projectile);
     }
